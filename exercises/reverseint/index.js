c2669cc90e0673284085777e2fa037e17eef5749
Jessie Sidhu <jsidhu22@gmail.com>
2019-07-31 13:32:27 -0700

    finished reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let str = '' + n;
+    let arr = str.split('');
+    let reversed = '';
+    if(arr[0] === '-'){
+        for(let i = arr.length - 1; i >= 1; i--){
+            reversed = reversed + arr[i];
+        }
+        reversed = '-' + reversed;
+        return parseInt(reversed);
+
+    } else {
+        return parseInt(str
+        .split('')
+        .reverse()
+        .join(''));
+    }
+}
 
 module.exports = reverseInt;
